0762d4985453fb9492983536f6200c9c1d43894b
AmirrezaFiroozi <firoozi128.af@gmail.com>
2016-06-03 23:44:10 +0430

    fix some bugs (esp on arch)
    i just fixed some bugs

--- installer.sh
+++ installer.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#author : Amirreza Firooz
+#author : Amirreza Firoozi
 #installer of wttr
 clear
 echo -e "******************************\nWelcome to installer of wttr \n *************************** \n Installing ..."
-sudo cp -R "wttr" /usr/bin
+sudo cp -R "wttr" /usr/bin  2>/dev/null 
  st_0=$?
 cd /usr/bin
 echo -e "******************************\n Giving permission ..."
-sudo chmod +x wttr
+sudo chmod +x /usr/bin/wttr 2>/dev/null 
  st_1=$?
 if [ $st_0 == 0 ] && [ $st_1 == 0 ];then
     echo -e "\n********* Congratulations!!! installation has just completed *********\n"
